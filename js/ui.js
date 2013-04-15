1fa84ca57eee9a0b918daefb3f1d0ed3e70ab256
RynoRn <horvathlaszlo88@gmail.com>
2013-04-15 16:55:51 +0200

    UI script refactor.

--- js/ui.js
+++ js/ui.js
@@ -1,144 +1,128 @@
-function initUI() {
+document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
 
-    var iScrolls = {
-        listScroll: null,
-        rightScroll: null
-    };
+var iScrolls = {
+    iScrollOptions: {
+        bounce: false,
+        lockDirection: true,
+        checkDOMChanges: true,
+        useTransform: true,
+        useTransition: true
+    },
+    listScroll: null,
+    listScrollInit: function () {
+        console.count("listScrollInit");
 
-    //================================================== FUNCTIONS ==================================================//
-    window.toggleMap = function toggleMap() {
-        var left = $(".left-column");
-        left.toggleClass("open");
-
-        var windowWidth = window.innerWidth,
-            windowHeight = window.innerHeight;
-
-        if (windowWidth < 480) {
-            if (left.hasClass("open")) {
-                document.getElementById("map").style.height = (windowHeight - 250) + "px";
-                setTimeout(function () {
-                    initListIScroll();
-                }, 350);
-            } else {
-                document.getElementById("map").style.height = windowHeight + "px";
-            }
-        } else if (windowWidth >= 480 || windowWidth <= 768) {
-            if (left.hasClass("open")) {
-                setTimeout(function () {
-                    document.getElementById("map").style.width = (windowWidth - $(".left-column").width()) + "px";
-                    $(".large-9")[0].style.width = (windowWidth - $(".left-column").width() - 1) + "px";
-                    document.getElementById("wrapper").style.height = (window.innerHeight - 67) + "px";
-                    $(".row-full .left-column")[0].style.height = window.innerHeight  + "px";
-                    initListIScroll();
-                }, 500);
-            } else {
-                document.getElementById("map").style.width = windowWidth + "px";
-                $(".large-9")[0].style.width = windowWidth + "px";
-            }
+        if (iScrolls.listScroll) {
+            //iScrolls.listScroll.destroy();
+            //iScrolls.listScroll = null;
+            iScrolls.listScroll.refresh();
         } else {
-            document.getElementById("map").style.width = (windowWidth - $(".left-column").width()) + "px";
+            iScrolls.listScroll = new iScroll('wrapper', iScrolls.iScrollOptions);
         }
-        lmap.invalidateSize();
-    }
+    },
+    rightScroll: null,
+    rightScrollInit: function () {
+        console.count("rightScrollInit");
 
-    window.hideRightPanel = function hideRightPanel() {
-        $(".right-panel").addClass("hidden");
-    }
-
-    window.showRightPanel = function showRightPanel() {
-        $(".right-panel").removeClass("hidden");
-
-        setTimeout(function () {
-            initRightIScroll();
-        }, 100);
-    }
-
-    window.toggleRightPanel = function toggleRightPanel() {
-        var right = $(".right-column");
-        right.toggleClass("hidden");
-
-        if (!right.hasClass("hidden")) {
-            setTimeout(function () {
-                initRightIScroll();
-            }, 100);
+        if (iScrolls.rightScroll) {
+            //iScrolls.rightScroll.destroy();
+            //iScrolls.rightScroll = null;
+            iScrolls.rightScroll.refresh();
+        } else {
+            iScrolls.rightScroll = new iScroll('wrapper-right', iScrolls.iScrollOptions);
         }
     }
+};
 
+function initUI() {
     var resizeTrigger;
     if (window.orientation === undefined) {
         window.onresize = function (event) {
             clearTimeout(resizeTrigger);
-            resizeTrigger = setTimeout(resizeFunction, 500);
+            resizeTrigger = setTimeout(function () { resizeUI(); }, 100);
         }
     } else {
         window.onorientationchange = function (event) {
             clearTimeout(resizeTrigger);
-            resizeTrigger = setTimeout(resizeFunction, 500);
+            resizeTrigger = setTimeout(function () { resizeUI(); }, 100);
         }
     }
-    function resizeFunction() {
-        resizeUI();
-    }
-
-    function initRightIScroll() {
-        if (iScrolls.rightScroll) {
-            iScrolls.rightScroll.destroy();
-            iScrolls.rightScroll = null;
-        }
-
-        iScrolls.rightScroll = new iScroll('wrapper-right', {
-            bounce: false,
-            lockDirection: true,
-            checkDOMChanges: true,
-            useTransform: true,
-            useTransition: true
-        });
-    }
-
-    function initListIScroll() {
-        if (iScrolls.listScroll) {
-            iScrolls.listScroll.destroy();
-            iScrolls.listScroll = null;
-        }
-
-        iScrolls.listScroll = new iScroll('wrapper', {
-            bounce: false,
-            lockDirection: true,
-            checkDOMChanges: true,
-            useTransform: true,
-            useTransition: true
-        });
-    }
-
-    resizeUI();
-
-    document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
-    initListIScroll();
-    //document.addEventListener('DOMContentLoaded', function () { setTimeout(initListIScroll, 1500); }, false);
 };
 
-function resizeUI() {
-    document.body.style.width = window.innerWidth + "px";
+window.hideRightPanel = function hideRightPanel() {
+    $(".right-panel").addClass("hidden");
+}
 
-    if (L.Browser.mobile) {
-        if (window.innerWidth < 480) {
-            document.getElementById("map").style.width = window.innerWidth + "px";
-        } else if (!$(".left-column").hasClass("open")) {
-            document.getElementById("map").style.width = window.innerWidth + "px";
-        }
-    } else {
-        document.getElementById("row-full").style.height = window.innerHeight + "px";
-        document.getElementById("wrapper").style.height = (window.innerHeight - 67) + "px";
-    }
-    if (window.innerWidth < 480) {
-        $(".left-column")[0].style.maxHeight = 250 + "px";
-    }
-    document.getElementById("map").style.height = window.innerHeight + "px";
-    $(".large-9")[0].style.width = (window.innerWidth - $(".left-column").width() - 1) + "px";
+window.showRightPanel = function showRightPanel() {
+    $(".right-panel").removeClass("hidden");
 
-    var maxH = document.querySelectorAll(".max-height-scroll");
-    for (var i = 0; i < maxH.length; i++) {
-        maxH[i].style.maxHeight = window.innerHeight + "px";
+    setTimeout(function () {
+        iScrolls.rightScrollInit();
+    }, 100);
+}
+
+window.toggleRightPanel = function toggleRightPanel() {
+    var right = $(".right-column");
+    right.toggleClass("hidden");
+
+    if (!right.hasClass("hidden")) {
+        setTimeout(function () {
+            iScrolls.rightScrollInit();
+        }, 100);
     }
 }
 
+window.resizeUI = function resizeUI() {
+    console.count("resize UI");
+
+    var mapHtml = document.getElementById("map"),
+        left = $('.left-column'),
+        large9 = $(".large-9")[0],
+        windowWidth = window.innerWidth,
+        windowHeight = window.innerHeight
+
+    document.body.style.width = window.innerWidth + "px";
+
+    if (windowWidth < 480) {
+        if (left.hasClass("open")) {
+            mapHtml.style.height = (windowHeight - 250) + "px";
+            large9.style.height = (windowHeight - 250) + "px";
+        } else {
+            mapHtml.style.height = windowHeight + "px";
+            large9.style.height = windowHeight + "px";
+        }
+        mapHtml.style.width = windowWidth + "px";
+        large9.style.width = windowWidth + "px";
+        left[0].removeAttribute("style");
+    }
+    else if (windowWidth >= 480 && windowWidth <= 768) {
+        if (left.hasClass("open")) {
+            mapHtml.style.width = (windowWidth - $(".left-column").width()) + "px";
+            large9.style.width = (windowWidth - $(".left-column").width() - 1) + "px";
+            left[0].style.height = windowHeight + "px";
+            document.getElementById("wrapper").style.height = (windowHeight - 67) + "px";
+        } else {
+            mapHtml.style.width = windowWidth + "px";
+            large9.style.width = windowWidth + "px";
+        }
+        mapHtml.style.height = windowHeight + "px";
+    }
+    else {
+        console.log("desktop");
+
+        mapHtml.style.width = (windowWidth - left.width()) + "px";
+        mapHtml.style.height = windowHeight + "px";
+        left[0].style.height = windowHeight + "px";
+        document.querySelectorAll(".row.collapse.relative")[0].style.height = (windowHeight - 67) + "px";
+    }
+
+    iScrolls.listScrollInit();
+    lmap.invalidateSize();
+}
+
+window.toggleMap = function toggleMap() {
+    $(".left-column").toggleClass("open");
+    $(".map").toggleClass("open");
+
+    resizeUI();
+}
