71cde6ebbd4b327951046db1e63c7d4a06c04fb4
RynoRn <horvathlaszlo88@gmail.com>
2013-04-15 11:04:40 +0200

    Resizing UI on window resize.

--- js/ui.js
+++ js/ui.js
@@ -1,25 +1,11 @@
 function initUI() {
 
-
     var iScrolls = {
         listScroll: null,
         rightScroll: null
     };
 
-    document.body.style.width = window.innerWidth + "px";
-    //================================================== SET MAP HEIGHT ==================================================//
-    if (L.Browser.mobile) {
-        //document.getElementById("map").style.height = (window.innerHeight - 250) + "px";
-    } else {
-        document.getElementById("row-full").style.height = window.innerHeight + "px";
-        document.getElementById("wrapper").style.height = window.innerHeight + "px";
-    }
-    document.getElementById("map").style.height = window.innerHeight + "px";
-    
-    var maxH = document.querySelectorAll(".max-height-scroll");
-    for (var i = 0; i < maxH.length; i++) {
-        maxH[i].style.maxHeight = window.innerHeight + "px";
-    }
+    resizeUI();
 
     //================================================== FUNCTIONS ==================================================//
     window.toggleMap = function toggleMap() {
@@ -59,6 +45,15 @@ function initUI() {
         }
     }
 
+    var resizeTrigger;
+    window.onresize = function(event) {
+        clearTimeout(resizeTrigger);
+        resizeTrigger = setTimeout(resizeFunction, 100);
+    }
+    function resizeFunction() {
+        resizeUI();
+    }
+
     function initRightIScroll() {
         if (iScrolls.rightScroll) {
             iScrolls.rightScroll.destroy();
@@ -89,6 +84,24 @@ function initUI() {
         });
     }
 
+    function resizeUI() {
+        document.body.style.width = window.innerWidth + "px";
+
+        if (L.Browser.mobile) {
+            //document.getElementById("map").style.height = (window.innerHeight - 250) + "px";
+        } else {
+            document.getElementById("row-full").style.height = window.innerHeight + "px";
+            document.getElementById("wrapper").style.height = (window.innerHeight - 67) + "px";
+        }
+        document.getElementById("map").style.height = window.innerHeight + "px";
+
+        var maxH = document.querySelectorAll(".max-height-scroll");
+        for (var i = 0; i < maxH.length; i++) {
+            maxH[i].style.maxHeight = window.innerHeight + "px";
+        }
+    }
+
     document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
     document.addEventListener('DOMContentLoaded', function () { setTimeout(initListIScroll(), 1500); }, false);
 };
+
